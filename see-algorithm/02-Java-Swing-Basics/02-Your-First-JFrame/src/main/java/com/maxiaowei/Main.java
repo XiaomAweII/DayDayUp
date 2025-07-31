a023b647b531757710b504135976a6eca45f247c
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-07-31 23:18:57 +0800

    2025/07/31

---
+++ see-algorithm/02-Java-Swing-Basics/02-Your-First-JFrame/src/main/java/com/maxiaowei/Main.java
@@ -0,0 +1,15 @@
+package com.maxiaowei;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+             AlgoFrame frame = new AlgoFrame("Welcome", 500, 500);
+//            AlgoFrame frame = new AlgoFrame("Welcome");
+        });
+    }
+}
---
+++ see-algorithm/02-Java-Swing-Basics/04-Using-Graphics2D/src/main/java/com/maxiaowei/Main.java
@@ -0,0 +1,13 @@
+package com.maxiaowei;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            AlgoFrame frame = new AlgoFrame("Welcome",500,500);
+        });
+    }
+}
